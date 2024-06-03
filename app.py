d188bb5d27da75862e04655bb2f2a351be0d920b
Yoni Lerner <yonilern@gmail.com>
2024-06-03 17:08:00 +0300

    Multiple players overhaul

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ import os
 import random
 import textwrap
 from collections import defaultdict
+from copy import deepcopy
+from uuid import uuid4
 import streamlit as st
+import streamlit.components.v1 as components
 
 # Load the wordpacks
 wordpacks = {}
@@ -22,51 +25,159 @@ for filename in os.listdir("wordpacks"):
             else:
                 wordpacks[wordpack] = lines
 
+# Helper for using icons in buttons
+# key_dict = {}
+# st.write('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"/>', unsafe_allow_html=True)
+
+
+# def generate_key(icon, button_key):
+#     key_dict[button_key] = icon
+#     return {'label': button_key, 'key': button_key}
+
+
 st.title("Sygil Word Generator")
 st.write("Select which wordpacks to include and the number of words to generate:")
 
-if "generated_words" not in st.session_state:
-    st.session_state.generated_words = [[]]
+if "generator_input" not in st.session_state:
+    st.session_state.generator_input = {
+        "schema_version": 1,
+        "players": [
+            {
+                "name": None,
+                "groups": [
+                    {
+                        "wordpacks": ["Basic"],
+                        "num_words": 10
+                    }
+                ]
+            }
+        ]}
+if "generator_output" not in st.session_state:
+    st.session_state.generator_output = []
+
+# generator_input schema:
+# {
+#   "schema_version": 1,
+#   "players": [
+#       {
+#           "name": str|None,
+#           "groups": [
+#               {
+#                   "wordpacks": [str],
+#                   "num_words": int
+#               },
+#           ],
+#       },
+#   ],
+# }
 
 
-def generate_words(groups):
-    generated_words = [[] for _ in range(num_players)]
-    for i in range(num_players):
-        for group in groups:
+def generate_v1(generator_input):
+    generator_output = []
+    for player_i, player in enumerate(generator_input["players"]):
+        player_output = {
+            "name": player["name"] or f"Player {player_i + 1}",
+            "words": [], "wordpack_origins": [], "group_origins": []
+        }
+        for group_i, group in enumerate(player["groups"]):
+            # Gather choices
             choices = []
             words = set()  # For deduplication
             for wordpack in group["wordpacks"]:
                 for word in wordpacks[wordpack]:
-                    if word in words:
+                    if word in words or word in player_output["words"]:
                         continue
-                    choices.append((word, wordpack))
+                    choices.append({"word": word, "wordpack_origin": wordpack, "group_origin": group_i})
                     words.add(word)
+
+            # Sample
             try:
-                generated_words[i] += random.sample(choices, group["num_words"])
+                player_output["words"] += random.sample(choices, group["num_words"])
             except ValueError:
-                st.error(f"Cannot generate {group['num_words']} unique words from wordpacks {group['wordpacks']}. Please reduce the number of words or add more wordpacks.")
+                st.error(f"Cannot generate {group['num_words']} unique words from wordpacks {group['wordpacks']}{'' if len(generator_input) == 1 else (' (' + player_output['name'] + ')')}. Please reduce the number of words or add more wordpacks.")
                 return
-    st.session_state.generated_words = generated_words
+        generator_output.append(player_output)
+    return generator_output
 
 
+def generate(generator_input):
+    if generator_input["schema_version"] == 1:
+        return generate_v1(generator_input)
+    st.error(f"Unsupported generator schema version: f{generator_input['schema_version']}")
+
+
+# st.button(**generate_key('<i class="fa-solid fa-circle-user fa-bounce"></i>', button_key='button_test_1'))
+# st.button(**generate_key('<i class="fa-brands fa-youtube fa-spin-pulse"></i>', button_key='button_test_2'))
+
+# Give each player an ID for key-generating purposes (otherwise you get issues with moving Streamlit components around)
+def generate_player_id(): return str(uuid4())[:8]
+
+
+for player in st.session_state.generator_input["players"]:
+    if "id" not in player:
+        player["id"] = generate_player_id()
+
+is_multiple_players = len(st.session_state.generator_input["players"]) > 1
 with st.container(border=True):
-    cols = st.columns([1, 2, 3, 1, 2])
-    cols[0].write("Players:")
-    num_players = cols[1].number_input("Number of players", min_value=1, value=1, label_visibility="collapsed")
-    cols[3].write("Groups:")
-    num_groups = cols[4].number_input("Number of groups", min_value=1, value=1, label_visibility="collapsed")
+    players = deepcopy(st.session_state.generator_input["players"])
+    for player_i, player in enumerate(players):
+        with st.container(border=True):
+            # Player name on left, buttons (up/down/clone/delete) on right
+            if is_multiple_players:
+                with st.container():
+                    cols = st.columns([10, 4, 1, 1, 1, 1])
+                    player["name"] = cols[0].text_input("Name",
+                                                        key=f"name_player-{player['id']}",
+                                                        value=player["name"], placeholder=f"Player {player_i + 1}", label_visibility="collapsed")
+                    if cols[2].button("↑", key=f"up_button_player-{player['id']}", disabled=player_i == 0):
+                        players.pop(player_i)
+                        players.insert(player_i - 1, player)
+                        st.session_state.generator_input["players"] = players
+                        st.rerun()
+                    if cols[3].button("↓", key=f"down_button_player-{player['id']}", disabled=player_i == len(st.session_state.generator_input["players"]) - 1):
+                        players.pop(player_i)
+                        players.insert(player_i + 1, player)
+                        st.session_state.generator_input["players"] = players
+                        st.rerun()
+                    if cols[4].button("C", key=f"clone_button_player-{player['id']}"):
+                        copy = deepcopy(player)
+                        if copy["name"]:
+                            copy["name"] += " (copy)"
+                        copy["id"] = generate_player_id()
+                        players.insert(player_i + 1, copy)
+                        st.session_state.generator_input["players"] = players
+                        st.rerun()
+                    if cols[5].button("X", key=f"delete_button_player-{player['id']}"):
+                        players.pop(player_i)
+                        st.session_state.generator_input["players"] = players
+                        st.rerun()
 
-    groups = []
-    for i in range(num_groups):
-        group = {}
-        cols = st.columns([2, 1, 6])
-        group["num_words"] = cols[0].number_input(f"Number of words", key=f"num_words_{i}", min_value=1, value=10, label_visibility="collapsed")
-        cols[1].write(" from ")
-        group["wordpacks"] = cols[2].multiselect(f"Wordpacks", key=f"wordpacks_{i}", options=list(wordpacks.keys()), default=["Basic"] if i == 0 else [], label_visibility="collapsed")
-        groups.append(group)
+            groups = []
+            for player_i, group in enumerate(player["groups"]):
+                group_values = {}
+                cols = st.columns([2, 1, 6])
+                group_values["num_words"] = cols[0].number_input(f"Number of words",
+                                                                 key=f"num_words_player-{player['id']}_group{player_i}",
+                                                                 min_value=1, value=group["num_words"], label_visibility="collapsed")
+                cols[1].write(" from ")
+                group_values["wordpacks"] = cols[2].multiselect(f"Wordpacks",
+                                                                key=f"wordpacks_player-{player['id']}_group{player_i}", options=list(wordpacks.keys()),
+                                                                default=group["wordpacks"], label_visibility="collapsed", placeholder="Choose wordpacks...")
+                groups.append(group_values)
+            player["groups"] = groups
 
-    if st.button("Generate Words"):
-        generate_words(groups)
+    cols = st.columns([15, 1])
+    if cols[1].button("\\+"):
+        st.session_state.generator_input["players"].append({
+            "name": None,
+            "groups": [{"wordpacks": [], "num_words": 1}],
+            "id": generate_player_id()
+        })
+        st.rerun()
+
+    if st.button("Generate"):
+        st.session_state.generator_input["players"] = players
+        st.session_state.generator_output = generate(st.session_state.generator_input)
 
 cols = st.columns(3)
 alphabetize = cols[0].checkbox("Alphabetize")
@@ -83,17 +194,33 @@ def render_list(l, pre=False):
         return "\n".join(f"* {word}" for word in l)
 
 
-for i in range(len(st.session_state.generated_words)):
-    display_words = st.session_state.generated_words[i]
+for player_i, player in enumerate(st.session_state.generator_output or []):
+    words = player["words"]
     if alphabetize:
-        display_words = sorted(display_words)
-    if len(st.session_state.generated_words) > 1:
-        st.markdown(f"### Player {i + 1}")
+        words = sorted(words, key=lambda w: w["word"])
+    if len(st.session_state.generator_output) > 1:
+        st.markdown("### " + player["name"])
     if do_group:
-        grouped_words = [(wordpack, [word for word, wpack in display_words if wpack == wordpack]) for wordpack, _ in sorted(wordpacks.items())]
         grouped_words = defaultdict(list)
-        for word, wordpack in display_words:
-            grouped_words[wordpack].append(word)
+        for word in words:
+            grouped_words[word["wordpack_origin"]].append(word["word"])
         st.markdown("\n".join(f"* {wordpack}:\n" + textwrap.indent(render_list(words, pre=True), "  ") for wordpack, words in sorted(grouped_words.items())))
     else:
-        st.markdown(render_list([word for word, _ in display_words]))
+        st.markdown(render_list([word["word"] for word in words]))
+
+# Logic for using icons in buttons
+# icon_config = f"""
+#         <script>
+#             var elements = window.parent.document.getElementsByClassName('css-x78sv8 eqr7zpz4');
+#             let dict = {json.dumps(key_dict)};
+#             let keys = Object.keys(dict);
+#             let icons = Object.values(dict);
+#             for (var i = 0; i < elements.length; ++i) {{
+#                 for (var j = 0; j < keys.length; ++j){{
+#                     if (elements[i].innerText == keys[j])
+#                     elements[i].innerHTML = icons[j];
+#                 }}
+#             }}
+#         </script>
+#         """
+# components.html(f"{icon_config}", height=0, width=0)
